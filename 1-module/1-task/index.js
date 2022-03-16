197b64a5a80b85b0dff90d11aa40f0b16bdd21ea
fhvfn94 <fhvfn94@gmail.com>
2022-03-17 03:10:14 +0400

    1-module/1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  if (n === 0 || n === 1) {
+    return result
+  }
+  for (let index = 1; index <= n; index++) {
+    result = result * index
+  }
+  return result;
 }
+
+factorial(0); // 1
+factorial(1); // 1
+factorial(3); // 6
+factorial(5); // 120
