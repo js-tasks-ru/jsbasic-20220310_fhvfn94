5b596a5f926cdc7227f5f3975ec0010f154b58c5
fhvfn94 <fhvfn94@gmail.com>
2022-08-14 16:08:06 +0300

    решил задачу прошел npm test 2-module/1-task/index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,22 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let r = 0;
+  for (const key in salaries) {
+    if (typeof salaries[key] === "number" && salaries[key] > 0 && salaries[key] != Infinity && salaries[key] != -Infinity) {
+      r += salaries[key];
+    }
+  }
+  return r;
 }
+
+console.log(
+  sumSalary(
+    (salaries = {
+      John: 1000,
+      Ann: 1600,
+      Pete: 1300,
+      month: "December",
+      currency: "USD",
+      isPayed: false,
+    })
+  )
+);
