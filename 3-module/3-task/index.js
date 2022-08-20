4d9e375fc24262d181bd9d9e68aa1a310c44a112
fhvfn94 <fhvfn94@gmail.com>
2022-08-20 21:54:02 +0300

    выполнил npm test без ошибок. Сделал задачу 3-module/3-task/index.js

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split("-")
+  let r = []
+  for(let i = 1; i < str.length; i++) {
+    r.push(str[i].charAt(0).toUpperCase() + str[i].slice(1))
+  }
+  return str[0] + r.join("")
 }
+
+camelize('background-color') == 'backgroundColor';
+camelize('list-style-image') == 'listStyleImage';
+camelize('-webkit-transition') == 'WebkitTransition';
+console.log(camelize('list-style-image'));
