b986c3557275405088c6103f5f49ee267ae4db35
fhvfn94 <fhvfn94@gmail.com>
2022-08-20 19:28:58 +0300

    npm test отработал без ошибок. Сделал зпдпние 3-module/2-task/index.js

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,11 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let r = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] >= a && arr[i] <= b){
+      r.push(arr[i])
+    }
+  }
+  return r
 }
+
+console.log(filterRange([5,3,8,1], 1, 4));
